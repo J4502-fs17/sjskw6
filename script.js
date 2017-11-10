aefafd8900fb39e85cb7811674f4a7111837f335
Samantha Stokes <samantha_stokes@outlook.com>
2017-11-09 22:41:15 -0600

    got the javascript to work TGOD

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-$('.menu-trigger').click(function() {
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
     $('nav ul').slideToggle(500);
   });
 
   $(window).resize(function() {
-	   if (  $(window).width() > 700 ) {
-		$('nav ul').removeAttr('style');
-	 }
-	});
+    if ( $(window).width() > 700 ) {
+    $('nav ul').removeAttr('style');
+    }
+  });
 });
